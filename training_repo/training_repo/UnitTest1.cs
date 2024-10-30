7b644d8375550b246f3d29ed8630846dab921467
Navneet Pal <Navneet.Pal@brandmuscle.com>
2024-10-30 11:51:11 +0530

    init

---
+++ training_repo/training_repo/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace training_repo
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //hello world
+            Assert.Pass();
+        }
+    }
+}
